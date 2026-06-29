9f71ac9f434b93bae82fcca7b2b24d1a51bafba6
Deeksha <deekshakavthar@gmail.com>
2026-06-29 16:44:53 +0530

    training

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -1,13 +1,19 @@
-import Welcome from "./components/Welcome";
 import NavBar from "./components/NavBar";
+import Welcome from "./components/Welcome";
+import Footer from "./components/Footer";
+import CompanyCard from "./components/CompanyCard";
+import JobCard from "./components/JobCard";
 
 function App() {
   return (
     <>
       <NavBar />
       <Welcome />
+      <CompanyCard />
+      <JobCard />
+      <Footer />
     </>
   );
 }
 
-export default App
+export default App;
