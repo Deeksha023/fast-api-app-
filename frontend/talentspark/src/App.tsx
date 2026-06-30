db6e8a9111efe72e279ec350d3fdd3109b6f7f5d
Deeksha <deekshakavthar@gmail.com>
2026-06-30 12:38:05 +0530

    frontend

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -3,13 +3,42 @@ import Welcome from "./components/Welcome";
 import Footer from "./components/Footer";
 import CompanyCard from "./components/CompanyCard";
 import JobCard from "./components/JobCard";
+import { useState, useEffect } from "react";
+import { getCompanies } from "./Services/CompanyService";
+import type { Company } from "./types/company";
 
 function App() {
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+  const [companies, setCompanies] = useState<Company[]>([]);
+
+  async function fetchCompanies() {
+
+    setLoading(true);
+
+    try {
+      const companies = await getCompanies();
+      setCompanies(companies);
+
+    } catch (err) {
+      setError(err as Error);
+
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchCompanies();
+  }, []);
+
   return (
     <>
       <NavBar />
       <Welcome />
-      <CompanyCard />
+      <br />
+      <CompanyCard key={companies.id} companies={companies} />
       <JobCard />
       <Footer />
     </>
---
+++ frontend/talentspark/src/types/job.ts
@@ -0,0 +1,7 @@
+export interface job {
+    id: number;
+    title: string;
+    description: string;
+    salary: string;
+    company_id: number;
+}
